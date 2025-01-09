abd176091a8000e0d30e1f4914d6775435e5a85c
DerickT02 <derickt18@outlook.com>
2025-01-08 16:13:57 -0800

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
-import  express  from 'express'
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+
+
+
 
 
 
 
 const app = express()
+app.use(morgan('dev'));
+app.use(cors())
 app.use(express.static('public'))
 app.get("/", (req: any, res: any) => {
-    console.log()
+    let host = req.origin
+    console.log(host) 
     res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(3000, () => {
     console.log("Server is running")
-}) 
+})  
